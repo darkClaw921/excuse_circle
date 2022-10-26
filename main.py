4ce633ddb12ac526519c131e8eb9a9cf4d6e198f
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2022-10-27 00:42:12 +0300

    работает

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-
+import random
 from loguru import logger
 from art import text2art
 from dotenv import load_dotenv
 from aiogram.types import InputFile
 from pprint import pprint
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
 
 from aiogram.types.message import ContentType
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
@@ -21,19 +24,40 @@ tgToken = os.environ.get('TG_TOKEN')
 bot = Bot(token=str(tgToken)) 
 dp = Dispatcher(bot,storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
-sticers = []
+sticers = ['CAACAgIAAxkBAAEGNfZjWZ3YTcWVtueIDDinJZwFiB5ZRQACUSAAAlLuyUoShx3sN6ZoKyoE',
+           'CAACAgIAAxkBAAEGNfhjWZ3xmhAsBY8S-jmARx0XIBYRpAACkB8AAtf4yErFKoGjiImKDyoE']
 
+markup_first = ReplyKeyboardMarkup(resize_keyboard=True).add(
+    KeyboardButton('Жги')
+)
 
+slova = ['помоги',
+'помогите',
+'памагите',
+'спасите',
+'помоги',
+'помощь',
+'помочь',
+'выручи',
+'выручай',
+'выручите',
+'выручку',
+'выручка',
+'жги']
 
-@dp.message_handler(content_types=ContentType.ANY)
+#@dp.message_handler(content_types=ContentType.ANY)
+@dp.message_handler(lambda msg: any(word in msg.text.lower() for word in slova))
 @logger.catch
 async def echo_message(msg: types.Message):
     state = dp.current_state(user=msg.from_user.id)
     
     message = msg.text.lower()
     user_id = msg.from_user.id
-    pprint(msg)
-    await bot.send_message(msg.from_user.id, 'лох')
+    #pprint(msg)
+
+    #await msg.answer_sticker(random.choice(sticers), reply_markup=markup_first)
+    await bot.send_sticker(msg.chat.id,random.choice(sticers), reply_markup=markup_first)
+    #await bot.send_message(msg.from_user.id, 'лох')
     #await state.set_state('firstMsg')
 
 
